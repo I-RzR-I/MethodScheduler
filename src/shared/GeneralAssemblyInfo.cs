fa94237912ee56134bdb99c71649ffc27e7b19a8
RzR <108324929+I-RzR-I@users.noreply.github.com>
2025-02-12 20:16:34 +0200

    Generate new version and changelog.

--- src/shared/GeneralAssemblyInfo.cs
+++ src/shared/GeneralAssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 #endif
 
-[assembly: AssemblyVersion("1.0.6.7128")]
-[assembly: AssemblyFileVersion("1.0.6.7128")]
-[assembly: AssemblyInformationalVersion("1.0.6.7128")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0.0")]
